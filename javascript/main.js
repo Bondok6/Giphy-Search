9a8317662d199ac0c82296350509664abe16feac
Bondok6 <kirolousbondok6@gmail.com>
2021-04-26 12:40:25 +0200

    final

--- javascript/main.js
+++ javascript/main.js
@@ -1,63 +1,50 @@
 // Grab the input value .
 
-document.querySelector(".js-go").addEventListener('click', () => {
+document.querySelector('.js-go').addEventListener('click', () => {
+	let input = document.querySelector('input');
+	let inputValue = input.value;
+	putInput(inputValue);
+});
 
-  let input = document.querySelector('input');
-  let inputValue = input.value;
-  putInput(inputValue);
-  
-})
+document.querySelector('.js-userinput').addEventListener('keyup', (e) => {
+	let input = document.querySelector('input');
+	let inputValue = input.value;
 
-document.querySelector(".js-userinput").addEventListener('keyup', (e) => {
-
-  let input = document.querySelector('input');
-  let inputValue = input.value;
-
-  if (e.which === 13) {
-    putInput(inputValue);
-  } 
-  
-})
-
-
+	if (e.which === 13) {
+		putInput(inputValue);
+	}
+});
 
 // do the data stuff with the API .
 function putInput(input) {
-  console.log(input);
-  let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=dc6zaTOxFJmzC`;
+	console.log(input);
+	let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=dc6zaTOxFJmzC`;
 
-  
-  // Ajax Call
-  let GiphyCallAjax = new XMLHttpRequest();
-  GiphyCallAjax.open('GET', url);
-  GiphyCallAjax.send();
+	// Ajax Call
+	let GiphyCallAjax = new XMLHttpRequest();
+	GiphyCallAjax.open('GET', url);
+	GiphyCallAjax.send();
 
-  GiphyCallAjax.addEventListener('load', (e) => {
-      let data = e.target.response;
-      console.log(data);
-      pushToDom(data);
-  })
-
+	GiphyCallAjax.addEventListener('load', (e) => {
+		let data = e.target.response;
+		console.log(data);
+		pushToDom(data);
+	});
 }
 
 putInput();
 
-
 // Show me the GIFs .
 
 function pushToDom(inputValue) {
+	let div = document.querySelectorAll('div')[1];
+	let response = JSON.parse(inputValue);
 
-  let div = document.querySelectorAll('div')[1];
-  let response = JSON.parse(inputValue);
+	div.innerHTML = '';
 
-  div.innerHTML = "";
+	response.data.forEach((el) => {
+		let src = el.images.fixed_height.url;
 
-  response.data.forEach(el => {
-    let src = el.images.fixed_height.url;
-    
-    div.innerHTML += `<img src = ${src} class="container-image"/>`;
-    
-  });
-  
-
+		div.innerHTML += `<img src = ${src} class="container-image"/>`;
+	});
 }
